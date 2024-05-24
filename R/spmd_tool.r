02c53c3f2aee765f15bc5e96f522d1da9e2037eb
George Ostrouchov <gostrouc@utk.edu>
2024-05-23 20:17:18 -0400

    three cats

--- R/spmd_tool.r
+++ R/spmd_tool.r
@@ -72,20 +72,28 @@ comm.print <- spmd.comm.print
 
 ## Constructs text decorations for spmd.comm.cat(). Includes debugging 
 ## rank-coloring option `quiet = "color"`.
+## 
+## Rewrite all.rank (and include specific rank)
+## - set specific ranks to all ranks so only one function is needed
+## - do a hand-off communication between the ranks instead of barrier for
+##   possible additional speedup and additional fractional delay between ranks.
+##   Currently, next rank prints immediately after barrier. Look for Drew's 
+##   version - he may have already done this!
+##   
 decor <- function(quiet, sep, rank) {
   if(is.logical(quiet)) {
     if(! quiet) {
-      m0 = paste0("COMM.RANK = ", rank, "\n")
-      m2 = ""
+      prefix = paste0("COMM.RANK = ", rank, "\n")
+      postfix = ""
     } else {
-      m0 = m2 = ""
+      prefix = postfix = ""
     }
   } else if(quiet == "color") {
     col = 90 + rank %% 8  # set ANSI text color codes (platform dependent)
-    m0 = paste0("\033[1;", col, "m")
-    m2 = "\033[0m" # reset color
+    prefix = paste0("\033[1;", col, "m")
+    postfix = "\033[0m" # reset color
   }
-  c(m0, m2)
+  c(prefix, postfix)
 }
 
 spmd.comm.cat <- function(..., all.rank = .pbd_env$SPMD.CT$print.all.rank,
@@ -103,7 +111,9 @@ spmd.comm.cat <- function(..., all.rank = .pbd_env$SPMD.CT$print.all.rank,
     for(i.rank in 0:(spmd.comm.size(comm) - 1)){
       if(i.rank == COMM.RANK){
         d = decor(quiet, sep, COMM.RANK)
-        cat(d[1], ..., d[2], sep = sep, fill = fill, labels = labels, append = append)
+        cat(d[1], sep = "", fill = fill, labels = labels, append = append)
+        cat(..., sep = sep, fill = fill, labels = labels, append = append)
+        cat(d[2], sep = "", fill = fill, labels = labels, append = append)
         if(flush){
           flush(con)
         }
