bfc2e41d896d27d57179bc6fb4db50dcdc366001
George Ostrouchov <gostrouc@utk.edu>
2024-05-25 16:52:33 -0400

    fixec LF

--- R/spmd_tool.r
+++ R/spmd_tool.r
@@ -18,15 +18,15 @@ spmd.hostinfo <- function(comm = .pbd_env$SPMD.CT$comm){
 #     flush = .pbd_env$SPMD.CT$msg.flush,
 #     barrier = .pbd_env$SPMD.CT$msg.barrier, con = stdout(), ...){
 #   COMM.RANK <- spmd.comm.rank(comm)
-# 
+#
 #   # Don't print "COMM.RANK = " even if verbose=TRUE in the case 'x' is invalid
 #   if (!exists(deparse(substitute(x))))
 #     quiet <- TRUE
-# 
+#
 #   if(barrier){
 #     spmd.barrier(comm)
 #   }
-# 
+#
 #   if(all.rank){
 #     for(i.rank in 0:(spmd.comm.size(comm) - 1)){
 #       if(i.rank == COMM.RANK){
@@ -64,14 +64,14 @@ spmd.hostinfo <- function(comm = .pbd_env$SPMD.CT$comm){
 #       }
 #     }
 #   }
-# 
+#
 #   invisible()
 # } # End of spmd.comm.print().
-  
-  
+
+
 ## Constructs text decorations for spmd.comm.cat().
 ## Includes debugging rank-coloring option quiet = "color".
-##   
+##
 decor <- function(quiet, comm) {
   if(is.logical(quiet)) {
     postfix <- ""
@@ -97,30 +97,30 @@ spmd.comm.print(x, all.rank = .pbd_env$SPMD.CT$print.all.rank,
                 sleep = 0, ...) {
   COMM.RANK <- spmd.comm.rank(comm)
   COMM.SIZE <- spmd.comm.size(comm)
-  
+
   ## Barrier is released by rank 0 after COMM.SIZE check-ins regardless of who
   ## posts them! Seems this might break in sub-comm grids?
   ## Prints are handed off to other system components so overprint can still
   ## occur - use a .01 or shorter sleep to avoid this.
   if(COMM.RANK == 0 && sleep > 0) Sys.sleep(sleep) # give last cat time to land
   if(barrier) spmd.barrier(comm)
-  
+
   ## If several ranks print, use distributed tag-team
   rank.print <- unique(rank.print) # duplicates would deadlock!
   if(all.rank) rank.print <- 0L:(COMM.SIZE - 1L)
   if(COMM.RANK %in% rank.print) {
     next.rank <- match(COMM.RANK, rank.print) + 1L
     prev.rank <- next.rank - 2L
-    
+
     if(prev.rank > 0L) # post a blocking receive
       recv(rank.source = rank.print[prev.rank], comm = comm)
-    
+
     d <- decor(quiet, comm)
     cat(d[1L], sep = "")
     print(x, ...)
     cat(d[2L], sep = "")
     if(flush) flush(con)
-    
+
     if(next.rank <= length(rank.print)) # release next print rank
       send(integer(0L), rank.dest = rank.print[next.rank], comm = comm)
   }
@@ -135,34 +135,34 @@ spmd.comm.cat <- function(..., all.rank = .pbd_env$SPMD.CT$print.all.rank,
     barrier = .pbd_env$SPMD.CT$msg.barrier, con = stdout(), sleep = 0) {
   COMM.RANK <- spmd.comm.rank(comm)
   COMM.SIZE <- spmd.comm.size(comm)
-  
-  ## Barrier is released by rank 0 after COMM.SIZE check-ins regardless of who 
+
+  ## Barrier is released by rank 0 after COMM.SIZE check-ins regardless of who
   ## posts them! Seems this might break in sub-comm grids?
   ## Prints are handed off to other system components so overprint can still
   ## occur - use a .01 or shorter sleep to avoid this.
   if(COMM.RANK == 0 && sleep > 0) Sys.sleep(sleep) # give last cat time to land
   if(barrier) spmd.barrier(comm)
-  
+
   ## If several ranks print, use distributed tag-team
   rank.print <- unique(rank.print) # duplicates would deadlock!
   if(all.rank) rank.print <- 0L:(COMM.SIZE - 1L)
   if(COMM.RANK %in% rank.print) {
     next.rank <- match(COMM.RANK, rank.print) + 1L
     prev.rank <- next.rank - 2L
-    
+
     if(prev.rank > 0L) # post a blocking receive
       recv(rank.source = rank.print[prev.rank], comm = comm)
-    
+
     d <- decor(quiet, comm)
     cat(d[1L], sep = "", fill = fill, labels = labels, append = append)
     cat(..., sep = sep, fill = fill, labels = labels, append = append)
     cat(d[2L], sep = "", fill = fill, labels = labels, append = append)
     if(flush) flush(con)
-    
+
     if(next.rank <= length(rank.print)) # release next print rank
       send(integer(0L), rank.dest = rank.print[next.rank], comm = comm)
   }
-  
+
   invisible()
 } # End of spmd.comm.cat().
 
