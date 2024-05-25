6d07df973850b3cc218efbc275ece7b04d82028a
George Ostrouchov <gostrouc@utk.edu>
2024-05-25 16:59:08 -0400

    test no sleep

--- R/spmd_tool.r
+++ R/spmd_tool.r
@@ -93,8 +93,7 @@ spmd.comm.print(x, all.rank = .pbd_env$SPMD.CT$print.all.rank,
                 rank.print = .pbd_env$SPMD.CT$rank.source,
                 comm = .pbd_env$SPMD.CT$comm, quiet = .pbd_env$SPMD.CT$print.quiet,
                 flush = .pbd_env$SPMD.CT$msg.flush,
-                barrier = .pbd_env$SPMD.CT$msg.barrier, con = stdout(),
-                sleep = 0, ...) {
+                barrier = .pbd_env$SPMD.CT$msg.barrier, con = stdout(), ...) {
   COMM.RANK <- spmd.comm.rank(comm)
   COMM.SIZE <- spmd.comm.size(comm)
 
