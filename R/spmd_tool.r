ed1a164291a5cc7ffa4c907367129b8701ea481d
George Ostrouchov <gostrouc@utk.edu>
2024-05-25 18:11:26 -0400

    typo

--- R/spmd_tool.r
+++ R/spmd_tool.r
@@ -93,7 +93,7 @@ spmd.comm.print <- function(x, all.rank = .pbd_env$SPMD.CT$print.all.rank,
                 rank.print = .pbd_env$SPMD.CT$rank.source,
                 comm = .pbd_env$SPMD.CT$comm, quiet = .pbd_env$SPMD.CT$print.quiet,
                 flush = .pbd_env$SPMD.CT$msg.flush,
-                barrier = .pbd.env$SPMD.CT$msg.barrier, con = stdout(), sleep = 0, ...) {
+                barrier = .pbd_env$SPMD.CT$msg.barrier, con = stdout(), sleep = 0, ...) {
   COMM.RANK <- spmd.comm.rank(comm)
   COMM.SIZE <- spmd.comm.size(comm)
 
