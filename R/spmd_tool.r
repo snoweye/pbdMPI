44e784ee5b4f47099444741ef33ad23c93b61fa0
George Ostrouchov <gostrouc@utk.edu>
2024-05-25 21:29:58 -0400

    cleanup

--- R/spmd_tool.r
+++ R/spmd_tool.r
@@ -12,67 +12,9 @@ spmd.hostinfo <- function(comm = .pbd_env$SPMD.CT$comm){
   invisible()
 } # End of spmd.hostinfo().
 
-spmd.comm.print <- function(x, all.rank = .pbd_env$SPMD.CT$print.all.rank,
-    rank.print = .pbd_env$SPMD.CT$rank.source, comm = .pbd_env$SPMD.CT$comm,
-    quiet = .pbd_env$SPMD.CT$print.quiet,
-    flush = .pbd_env$SPMD.CT$msg.flush,
-    barrier = .pbd_env$SPMD.CT$msg.barrier, con = stdout(), ...){
-  COMM.RANK <- spmd.comm.rank(comm)
-
-  # Don't print "COMM.RANK = " even if verbose=TRUE in the case 'x' is invalid
-  if (!exists(deparse(substitute(x))))
-    quiet <- TRUE
-
-  if(barrier){
-    spmd.barrier(comm)
-  }
-
-  if(all.rank){
-    for(i.rank in 0:(spmd.comm.size(comm) - 1)){
-      if(i.rank == COMM.RANK){
-        if(! quiet){
-          cat("COMM.RANK = ", COMM.RANK, "\n", sep = "")
-          if(flush){
-            flush(con)
-          }
-        }
-        print(x, ...)
-        if(flush){
-          flush(con)
-        }
-      }
-      if(barrier){
-        spmd.barrier(comm)
-      }
-    }
-  } else{
-    for(i.rank in rank.print){
-      if(i.rank == COMM.RANK){
-        if(! quiet){
-          cat("COMM.RANK = ", COMM.RANK, "\n", sep = "")
-          if(flush){
-            flush(con)
-          }
-        }
-        print(x, ...)
-        if(flush){
-          flush(con)
-        }
-      }
-      if(barrier){
-        spmd.barrier(comm)
-      }
-    }
-  }
-
-  invisible()
-} # End of spmd.comm.print().
-
-
 ## Constructs text decorations for spmd.comm.cat().
-## Includes debugging rank-coloring option quiet = "color".
-##
-decor <- function(quiet, comm) {
+## Includes debugging rank-coloring option quiet = comm.
+spmd.decor <- function(quiet, comm) {
   if(is.logical(quiet)) {
     postfix <- ""
     if(quiet) {
@@ -97,6 +39,10 @@ spmd.comm.print <- function(x, all.rank = .pbd_env$SPMD.CT$print.all.rank,
   COMM.RANK <- spmd.comm.rank(comm)
   COMM.SIZE <- spmd.comm.size(comm)
 
+  ## Don't print "COMM.RANK = " if 'x' is invalid
+  if (!exists(deparse(substitute(x))))
+    quiet <- TRUE
+
   ## Barrier is released by rank 0 after COMM.SIZE check-ins regardless of who
   ## posts them! Seems this might break in sub-comm grids?
   ## Prints are handed off to other system components so overprint can still
@@ -114,7 +60,7 @@ spmd.comm.print <- function(x, all.rank = .pbd_env$SPMD.CT$print.all.rank,
     if(prev.rank > 0L) # post a blocking receive
       recv(rank.source = rank.print[prev.rank], comm = comm)
 
-    d <- decor(quiet, comm)
+    d <- spmd.decor(quiet, comm)
     cat(d[1L], sep = "")
     print(x, ...)
     cat(d[2L], sep = "")
@@ -152,7 +98,7 @@ spmd.comm.cat <- function(..., all.rank = .pbd_env$SPMD.CT$print.all.rank,
     if(prev.rank > 0L) # post a blocking receive
       recv(rank.source = rank.print[prev.rank], comm = comm)
 
-    d <- decor(quiet, comm)
+    d <- spmd.decor(quiet, comm)
     cat(d[1L], sep = "", fill = fill, labels = labels, append = append)
     cat(..., sep = sep, fill = fill, labels = labels, append = append)
     cat(d[2L], sep = "", fill = fill, labels = labels, append = append)
